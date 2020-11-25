2f39d13bb0d5be9ae0b2866e77c82055e745fe40
Scott Hannabuss <scotthannabuss@hotmail.com>
2020-11-25 14:09:10 +0000

    added message alert when sship sails or docks

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,10 @@
 (function exportController() {
-    function Controller() {
-        this.initialiseSea()
+    function Controller(ship) {
+        this.ship = ship;
+        this.initialiseSea();
+        document.querySelector('#sailbutton').addEventListener('click', () => {
+            this.setSail()
+        });
     }
 
     Controller.prototype.initialiseSea = function initaliseSea() {
@@ -29,19 +33,59 @@
         });
     }
 
-    Controller.prototype.renderShip = function (ship) {
+    Controller.prototype.renderShip = function () {
+        const ship = this.ship
         const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
         const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+        const shipElement = document.querySelector('#ship');
         shipElement.style.top = `${portElement.offsetTop}px`;
         shipElement.style.left = `${portElement.offsetLeft}px`;
         shipElement.style.top = `${portElement.offsetTop + 32}px`;
         shipElement.style.left = `${portElement.offsetLeft - 32}px`;
-        const shipElement = document.querySelector('#ship');
     };
+
+    Controller.prototype.setSail = function () {
+        const ship = this.ship
+        const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+        const nextPortIndex = currentPortIndex >= 0 ? currentPortIndex + 1 : undefined;
+        const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+        const isLastPort = currentPortIndex === ship.itinerary.ports.length - 1;
+        if (!nextPortElement || isLastPort) {
+            return alert('End of the line!');
+        }
+        this.renderMessage(`Now departing ${ship.currentPort.name}`);
+
+        const shipElement = document.querySelector('#ship');
+        const sailInterval = setInterval(() => {
+            const shipLeft = parseInt(shipElement.style.left, 10);
+            if (shipLeft === (nextPortElement.offsetLeft - 32)) {
+                ship.setSail();
+                ship.dock();
+                this.renderMessage(`Docked at ${ship.currentPort.name}`);
+                clearInterval(sailInterval);
+            }
+
+            shipElement.style.left = `${shipLeft + 1}px`;
+        }, 20);
+    };
+
+    Controller.prototype.renderMessage = function (message) {
+        const messageElement = document.createElement('div');
+        messageElement.id = 'message';
+        messageElement.innerHTML = message;
+
+        const viewport = document.querySelector('#viewport');
+        viewport.appendChild(messageElement);
+
+        setTimeout(() => {
+            viewport.removeChild(messageElement);
+        }, 2000);
+    }
 
     if (typeof module !== 'undefined' && module.exports) {
         module.exports = Controller;
     } else {
         window.Controller = Controller;
     }
+
 }());
